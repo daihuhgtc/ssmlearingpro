4b4e3fc4c1a9078041eda01e3f030b703cbc9873
daihu <daihuhgtc@outlook.com>
2021-03-21 21:59:53 +0800

    init

---
+++ src/main/java/com/dh/ssm/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.dh.ssm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "hello world";
+    }
+}
